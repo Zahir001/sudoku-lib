5cc6f41ae4d6399706918fdc7b367eb0a2834716
Sagir42 <mdsagiralam42@gmail.com>
2023-12-12 12:55:31 +0530

    api intergated.

--- index.js
+++ index.js
@@ -1,39 +1,174 @@
-const sudokuQuestion = [
-  [5, 3, 0, 0, 7, 0, 0, 0, 0],
-  [6, 0, 0, 1, 9, 5, 0, 0, 0],
-  [0, 9, 8, 0, 0, 0, 0, 6, 0],
-  [8, 0, 0, 0, 6, 0, 0, 0, 3],
-  [4, 0, 0, 8, 0, 3, 0, 0, 1],
-  [7, 0, 0, 0, 2, 0, 0, 0, 6],
-  [0, 6, 0, 0, 0, 0, 2, 8, 0],
-  [0, 0, 0, 4, 1, 9, 0, 0, 5],
-  [0, 0, 0, 0, 8, 0, 0, 7, 9]
-];
+var dlevel=1;
+var levelItem = [35, 45, 50, 55, 60, 65];
+var question;
+var answer;
 
-const sudokuAnswer = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
-];
+// Backend Logic
 
-window.addEventListener('load', function() {
-  loadSuduko();
-})
+function StartPlaying() {
+  dlevel = document.getElementById("difficultySelect").value;
+  myFunction(dlevel);
 
-function loadSuduko() {
+  loadSudukoUI();
+}
+
+function myFunction(level) {
+    
+    //process question & answer array
+    var arr = [		
+        [[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]],[[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]],[[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]]
+    ];
+
+    sudukoSolver(arr, 0,0,0,0);
+
+    answer = arrayConversion(arr); //2d
+    
+    question = arrayCopy(answer);
+    createQuestion(level, question);
+}
+
+function arrayCopy(arr){
+    var temp = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+    for(var i =0; i<9; i++){
+        for(var j=0; j<9; j++){
+            temp[i][j]=arr[i][j];
+        }
+    }
+    return temp;
+}
+
+function arrayConversion(arr){
+    
+		var question=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+		var row = 0;
+        var col = 0;
+
+		for(var i=0; i<3; i++) {
+			for(var j=0; j<3; j++) {
+				for(var k=0; k<3; k++) {
+					for(var l=0; l<3; l++) {
+						if(col==9) {
+							row++;
+							col=0;
+						}
+						question[row][col] = arr[i][j][k][l];
+						col++;
+					}
+				}
+			}
+		}
+		return question;
+}
+
+function createQuestion(level,arr){
+    var row = 0, col = 0, min = 0, max = 8;
+    for(var i=0; i<levelItem[level-1]; ) {
+        row = getRndInteger(min,max);
+        col = getRndInteger(min,max);
+        if(arr[row][col] != 0 ) {
+            arr[row][col] = 0;
+            i++;
+        }
+    }
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+function arrayManipulate(arr){
+    var arr1 = arr;
+    for(var i=0; i<3; i++) {
+        for(var j=0; j<3; j++) {
+            for(var k=0; k<3; k++) {
+                for(var l=0; l<3; l++) {
+                    arr1[i][j][k][l]=0;
+                }
+            }
+        }
+    }
+}
+
+
+function sudukoSolver(arr, mainRow, mainCol, row, col){
+		if(col>2) {
+			row++;
+			col=0;
+		}
+		if(row>2) {
+			mainCol++;
+			row=0;
+		}
+		if(mainCol>2) {
+			mainRow++;
+			mainCol=0;
+		}
+		if(mainRow>2) {
+			return true;
+		}
+
+		if(arr[mainRow][row][mainCol][col]>0) 
+			return sudukoSolver(arr, mainRow, mainCol, row, col+1);
+
+        var arr1 = [1,2,3,4,5,6,7,8,9];
+        shuffle(arr1);
+
+		for(var i=0; i<9; i++) {
+            var elm = arr1[i];
+			if(isValid(arr, mainRow, mainCol, row, col, elm)) {
+				arr[mainRow][row][mainCol][col]=elm;
+				if(sudukoSolver(arr, mainRow, mainCol, row, col+1))
+					return true;
+				arr[mainRow][row][mainCol][col]=0;
+			}
+		}
+
+		return false;
+}
+ 
+// 4D array input
+function isValid(arr, mainRow, mainCol, row, col, num){
+    for(var i=0; i<3; i++) {
+        for(var j=0; j<3; j++) {
+            if(arr[mainRow][row][i][j]==num || arr[mainRow][i][mainCol][j]==num || arr[i][j][mainCol][col]==num){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+// UI Logic
+
+function refreshPressed() {
+  loadSudukoUI();
+}
+
+function resetPressed() {
+  document.getElementById('playContainer').style.display = 'block';
+  document.getElementById('reset').style.display = 'none';
+  document.getElementById('refresh').style.display = 'none';
+  document.getElementById('sudokuContainer').style.display = 'none';
+}
+
+function loadSudukoUI() {
+  document.getElementById('playContainer').style.display = 'none';
+  document.getElementById('reset').style.display = 'block';
+  document.getElementById('refresh').style.display = 'block';
+  document.getElementById('sudokuContainer').style.display = 'block';
+
   var sdk = document.getElementById('mySuduko');
   sdk.innerHTML = '';
   for (let i = 0; i < 9; i++){
     for (let j = 0; j < 9; j++){
-      var num = sudokuQuestion[i][j].toString();
+      var num = question[i][j].toString();
       var isEditable = 'readonly';
-      if (num == 0) {
+      if (num == '0') {
         isEditable = '';
         num = ''
       }
@@ -64,7 +199,7 @@ function numberTyped(i, j, elmId){
     return;
   } 
   
-  if (sudokuAnswer[i][j] == elmId.value) {
+  if (answer[i][j] == elmId.value) {
     // success
     elmId.style.color = 'black';
   } else {
