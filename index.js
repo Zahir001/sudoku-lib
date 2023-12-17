0870af87b9d21564b68820de3954af0a1762d201
Zahir001 <mdzahir0123@gmail.com>
2023-12-17 20:22:53 +0530

    changes in css desgin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var dlevel=1;
+var dlevel = 1;
 var levelItem = [30, 35, 40, 45, 50];
 var question;
 var answer;
@@ -13,145 +13,145 @@ var activeJ;
 function StartPlaying() {
   dlevel = document.getElementById("difficultySelect").value;
   myFunction(dlevel);
-  
+
   loadSudukoUI();
 }
 
 function myFunction(level) {
-    
-    //process question & answer array
-    var arr = [		
-        [[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]],[[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]],[[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]]
-    ];
 
-    sudukoSolver(arr, 0,0,0,0);
+  //process question & answer array
+  var arr = [
+    [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+  ];
 
-    answer = arrayConversion(arr); //2d
-    
-    question = arrayCopy(answer);
-    createQuestion(level, question);
+  sudukoSolver(arr, 0, 0, 0, 0);
+
+  answer = arrayConversion(arr); //2d
+
+  question = arrayCopy(answer);
+  createQuestion(level, question);
 }
 
-function arrayCopy(arr){
-    var temp = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
-    for(var i =0; i<9; i++){
-        for(var j=0; j<9; j++){
-            temp[i][j]=arr[i][j];
-        }
+function arrayCopy(arr) {
+  var temp = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      temp[i][j] = arr[i][j];
     }
-    return temp;
+  }
+  return temp;
 }
 
-function arrayConversion(arr){
-    
-		var question=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
-		var row = 0;
-        var col = 0;
+function arrayConversion(arr) {
 
-		for(var i=0; i<3; i++) {
-			for(var j=0; j<3; j++) {
-				for(var k=0; k<3; k++) {
-					for(var l=0; l<3; l++) {
-						if(col==9) {
-							row++;
-							col=0;
-						}
-						question[row][col] = arr[i][j][k][l];
-						col++;
-					}
-				}
-			}
-		}
-		return question;
-}
+  var question = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+  var row = 0;
+  var col = 0;
 
-function createQuestion(level,arr){
-    var row = 0, col = 0, min = 0, max = 8;
-    for(var i=0; i<levelItem[level-1]; ) {
-        row = getRndInteger(min,max);
-        col = getRndInteger(min,max);
-        if(arr[row][col] != 0 ) {
-            arr[row][col] = 0;
-            i++;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      for (var k = 0; k < 3; k++) {
+        for (var l = 0; l < 3; l++) {
+          if (col == 9) {
+            row++;
+            col = 0;
+          }
+          question[row][col] = arr[i][j][k][l];
+          col++;
         }
+      }
     }
+  }
+  return question;
+}
+
+function createQuestion(level, arr) {
+  var row = 0, col = 0, min = 0, max = 8;
+  for (var i = 0; i < levelItem[level - 1];) {
+    row = getRndInteger(min, max);
+    col = getRndInteger(min, max);
+    if (arr[row][col] != 0) {
+      arr[row][col] = 0;
+      i++;
+    }
+  }
 }
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function arrayManipulate(arr) {
+  var arr1 = arr;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      for (var k = 0; k < 3; k++) {
+        for (var l = 0; l < 3; l++) {
+          arr1[i][j][k][l] = 0;
+        }
+      }
+    }
+  }
+}
+
+
+function sudukoSolver(arr, mainRow, mainCol, row, col) {
+  if (col > 2) {
+    row++;
+    col = 0;
+  }
+  if (row > 2) {
+    mainCol++;
+    row = 0;
+  }
+  if (mainCol > 2) {
+    mainRow++;
+    mainCol = 0;
+  }
+  if (mainRow > 2) {
+    return true;
   }
 
-function arrayManipulate(arr){
-    var arr1 = arr;
-    for(var i=0; i<3; i++) {
-        for(var j=0; j<3; j++) {
-            for(var k=0; k<3; k++) {
-                for(var l=0; l<3; l++) {
-                    arr1[i][j][k][l]=0;
-                }
-            }
-        }
+  if (arr[mainRow][row][mainCol][col] > 0)
+    return sudukoSolver(arr, mainRow, mainCol, row, col + 1);
+
+  var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  shuffle(arr1);
+
+  for (var i = 0; i < 9; i++) {
+    var elm = arr1[i];
+    if (isValid(arr, mainRow, mainCol, row, col, elm)) {
+      arr[mainRow][row][mainCol][col] = elm;
+      if (sudukoSolver(arr, mainRow, mainCol, row, col + 1))
+        return true;
+      arr[mainRow][row][mainCol][col] = 0;
     }
+  }
+
+  return false;
 }
 
-
-function sudukoSolver(arr, mainRow, mainCol, row, col){
-		if(col>2) {
-			row++;
-			col=0;
-		}
-		if(row>2) {
-			mainCol++;
-			row=0;
-		}
-		if(mainCol>2) {
-			mainRow++;
-			mainCol=0;
-		}
-		if(mainRow>2) {
-			return true;
-		}
-
-		if(arr[mainRow][row][mainCol][col]>0) 
-			return sudukoSolver(arr, mainRow, mainCol, row, col+1);
-
-        var arr1 = [1,2,3,4,5,6,7,8,9];
-        shuffle(arr1);
-
-		for(var i=0; i<9; i++) {
-            var elm = arr1[i];
-			if(isValid(arr, mainRow, mainCol, row, col, elm)) {
-				arr[mainRow][row][mainCol][col]=elm;
-				if(sudukoSolver(arr, mainRow, mainCol, row, col+1))
-					return true;
-				arr[mainRow][row][mainCol][col]=0;
-			}
-		}
-
-		return false;
-}
- 
 // 4D array input
-function isValid(arr, mainRow, mainCol, row, col, num){
-    for(var i=0; i<3; i++) {
-        for(var j=0; j<3; j++) {
-            if(arr[mainRow][row][i][j]==num || arr[mainRow][i][mainCol][j]==num || arr[i][j][mainCol][col]==num){
-                return false;
-            }
-        }
+function isValid(arr, mainRow, mainCol, row, col, num) {
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (arr[mainRow][row][i][j] == num || arr[mainRow][i][mainCol][j] == num || arr[i][j][mainCol][col] == num) {
+        return false;
+      }
     }
+  }
 
-    return true;
+  return true;
 }
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+  array.sort(() => Math.random() - 0.5);
 }
 
 // UI Logic
 
-function parseIndexFromID(elmId){
-  var text = elmId.replace('sudukoItem_','');
+function parseIndexFromID(elmId) {
+  var text = elmId.replace('sudukoItem_', '');
   return [Array.from(text)[0], Array.from(text)[2]];
 }
 
@@ -166,19 +166,19 @@ function resetPressed() {
 
 function loadSudukoUI() {
   activeElm = null
-  
+
   document.getElementById('menuContainer').style.display = 'none';
   document.getElementById('gameContainer').style.display = 'block';
 
-  reqNumber = levelItem[dlevel-1];
+  reqNumber = levelItem[dlevel - 1];
 
   mistake = 0;
-  document.getElementById('mistakeCount').innerHTML = 'Mistake: '+mistake+' / 5';
+  document.getElementById('mistakeCount').innerHTML = 'Mistake: ' + mistake + ' / 5';
 
   var sdk = document.getElementById('mySuduko');
   sdk.innerHTML = '';
-  for (let i = 0; i < 9; i++){
-    for (let j = 0; j < 9; j++){
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
       var num = question[i][j].toString();
       if (num == '0') {
         num = ''
@@ -188,30 +188,30 @@ function loadSudukoUI() {
       if (j % 3 == 2 && j != 8) {
         style = 'border-right: 2px solid black;z-index: 1;';
       }
-      if (i % 3 == 2 && i != 8){
+      if (i % 3 == 2 && i != 8) {
         style += 'border-bottom: 2px solid black;';
       }
 
-      var elmId = 'sudukoItem_'+i+'_'+j;
-      sdk.innerHTML += '<input id="'+elmId+'" class="sudoku_child_item" type="tel" style="'+style+'" value="'+num+'" maxlength="1" readonly onclick="itemClicked('+i+','+j+','+elmId+')">';
-    } 
+      var elmId = 'sudukoItem_' + i + '_' + j;
+      sdk.innerHTML += '<input id="' + elmId + '" class="sudoku_child_item" type="tel" style="' + style + '" value="' + num + '" maxlength="1" readonly onclick="itemClicked(' + i + ',' + j + ',' + elmId + ')">';
+    }
   }
 
   updateBackgroundAndInputs();
 }
 
-function updateBackgroundAndInputs(elm){
-  var numInputs = [9,9,9,9,9,9,9,9,9];
+function updateBackgroundAndInputs(elm) {
+  var numInputs = [9, 9, 9, 9, 9, 9, 9, 9, 9];
 
-  for (let i = 0; i < 9; i++){
-    for (let j = 0; j < 9; j++){
-      const currElm = document.getElementById('sudukoItem_'+i+'_'+j);
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const currElm = document.getElementById('sudukoItem_' + i + '_' + j);
       if (currElm.value != '') {
         numInputs[parseInt(currElm.value) - 1] -= 1;
       }
-      if (elm != null){
-        if(elm.value == currElm.value) {
-          if (elm.value == ''){
+      if (elm != null) {
+        if (elm.value == currElm.value) {
+          if (elm.value == '') {
             currElm.style.background = 'white';
           } else {
             currElm.style.background = 'lightskyblue';
@@ -230,27 +230,27 @@ function updateBackgroundAndInputs(elm){
 
   for (let i = 0; i < numInputs.length; i++) {
     var visibility = '';
-    if (numInputs[i] == 0){
+    if (numInputs[i] == 0) {
       visibility = 'hidden';
     }
-    buttonDiv.innerHTML += '<button style="font-size: large; visibility:'+visibility+'" onclick="numberTyped('+(i+1)+')">'+(i+1)+'<div style="font-size: x-small;">'+numInputs[i]+'</div></button>';
+    buttonDiv.innerHTML += '<button style="visibility:' + visibility + '" onclick="numberTyped(' + (i + 1) + ')">' + (i + 1) + '<div style="font-size: 12px;">' + numInputs[i] + '</div></button>';
   }
 }
 
-function showDetailHighlited(i, j){
-  var elem = document.getElementById('sudukoItem_'+i+'_'+j);
-  if(elem.style.background == 'white'){
+function showDetailHighlited(i, j) {
+  var elem = document.getElementById('sudukoItem_' + i + '_' + j);
+  if (elem.style.background == 'white') {
     elem.style.background = 'lightgray';
   }
 }
 
 function showHighlited() {
-  if (activeElm == null){
+  if (activeElm == null) {
     return
   }
   var cId = parseIndexFromID(activeElm.id);
 
-  for(let i = 0; i < 9; i++){
+  for (let i = 0; i < 9; i++) {
     //rows
     showDetailHighlited(cId[0], i);
     //columns
@@ -261,17 +261,17 @@ function showHighlited() {
     var c = cId[1] / 3;
     c = parseInt(c) * 3;
     //3x3 box
-    showDetailHighlited(r+parseInt(i/3), c+(i%3));
+    showDetailHighlited(r + parseInt(i / 3), c + (i % 3));
   }
 }
-function itemClicked(i, j, elm){
+function itemClicked(i, j, elm) {
   activeElm = elm;
   activeI = i;
   activeJ = j;
   updateBackgroundAndInputs(elm);
 }
 
-function numberTyped(posV){
+function numberTyped(posV) {
   if (activeElm == null) {
     return
   }
@@ -281,12 +281,12 @@ function numberTyped(posV){
   }
 
   activeElm.value = posV;
-  
+
   if (answer[activeI][activeJ] == activeElm.value) {
     // success
     reqNumber--;
     activeElm.style.color = 'black';
-    if(reqNumber==0){
+    if (reqNumber == 0) {
       alert('congratulation!!! Retuning to main menu.');
       resetPressed();
     }
@@ -294,8 +294,8 @@ function numberTyped(posV){
   } else {
     // fail
     mistake++;
-    document.getElementById('mistakeCount').innerHTML = 'Mistake: '+mistake+' / 5';
-    if(mistake  >= 5 || mistake <= 0){
+    document.getElementById('mistakeCount').innerHTML = 'Mistake: ' + mistake + ' / 5';
+    if (mistake >= 5 || mistake <= 0) {
       // document.getElementById('mistakeCount').innerHTML = 'Mistake: '+mistake+' / 5';
       alert('Maximum mistake limit reached. Refreshing Suduko !!!');
       refreshPressed();
@@ -317,3 +317,42 @@ function erasePressed() {
 
   updateBackgroundAndInputs();
 }
+
+window.addEventListener("load", function(){
+  getAPIData();
+});
+
+
+// function getAPIData() {
+//   const apiUrl = 'https://jsonplaceholder.typicode.com/todos/';
+//   const outputElement = document.getElementById('output');
+
+//   fetch(apiUrl)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       // Display data in an HTML element
+//       outputElement.innerHTML = '';
+//       // outputElement.textContent += JSON.stringify(data, null, 2);
+//       for (let i = 0; i < data.length; i++) {
+//         const element = data[i];
+//         var backClr = element["completed"] == true ? 'green' : 'yellow';
+
+//         var idElm = '';
+//         if (element["completed"] == true){
+//           idElm = '';
+//         } else {
+//           idElm = ', userID:'+element["userId"]+', id:'+element["id"];
+//         }
+
+//         outputElement.innerHTML += '<div class="userDetails" style="background: '+backClr+';">'+element["title"]+idElm+'</div>';
+//       }
+//     })
+//     .catch(error => {
+//       console.log('Error:' + error);
+//     });
+// }
